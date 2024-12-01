18754715e65c1a67286673a144bfaa343730053e
wangyuankun <wangyuankun@aliyun.com>
2024-12-01 17:09:41 +0800

    Updated condition.

--- C/program_example/judge/condition.c
+++ C/program_example/judge/condition.c
@@ -4,6 +4,6 @@ int main() {
     int num;
     printf("Input a number: ");
     scanf("%d", &num);
-    (num % 2 == 0)?printf("even"):printf("odd");
-    printf("\n");
+    (num % 2 == 0) ? printf("even") : printf("odd");
+    printf("num is: %d\n", num);
 }
