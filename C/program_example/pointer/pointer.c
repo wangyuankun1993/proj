8295707a76e0564c822066db53672e71b0d79a48
wangyuankun <wangyuankun@aliyun.com>
2025-01-01 22:58:19 +0800

    Updated pointer.

--- C/program_example/pointer/pointer.c
+++ C/program_example/pointer/pointer.c
@@ -1,11 +1,22 @@
+// /usr/bin/gcc
+
+// File Name       : pointer.c
+// Author          : Yuankun.Wang
+// Created Time    : 2025-01-01 21:28:08
+// Last Modified   : 2025-01-01 22:53:16
 #include <stdio.h>
 
-int main() {
-    int var = 20;
+int main()
+{
+    int var = 10;
+    int *ptr = NULL;
     int *ip;
     ip = &var;
     printf("var address: %p\n", &var);
-    printf("ip saved address: %p\n", ip);
-    printf("ip value: %d\n", *ip);
+    printf("ip storage address: %p\n", ip);
+    printf("pointer access value: %d\n", *ip);
+    printf("ptr address: %p\n", ptr);
+    printf("ptr access value: %d\n", *ptr);
+
     return 0;
 }
