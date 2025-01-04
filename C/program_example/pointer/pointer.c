e64ba463620c07a9461bc055f5cff74e79aefae3
wangyuankun <wangyuankun@aliyun.com>
2025-01-05 00:01:21 +0800

    Updated pointer.

--- C/program_example/pointer/pointer.c
+++ C/program_example/pointer/pointer.c
@@ -3,16 +3,28 @@
 // File Name       : pointer.c
 // Author          : Yuankun.Wang
 // Created Time    : 2025-01-01 21:28:08
-// Last Modified   : 2025-01-02 23:51:12
+// Last Modified   : 2025-01-04 23:59:59
 #include <stdio.h>
+
+double getAverage(int *arr, int size);
 
 int main()
 {
-    char str[] = "Hello";
-    char *ptr = &str[4];
-    printf("initial: %c\n", *ptr);
-    ptr--;
-    printf("initial: %c\n", *ptr);
+    int balance[5] = {1000, 2, 3, 17, 50};
+    double avg;
+    avg = getAverage(balance, 5);
+    printf("Average vlaue is: %f\n", avg);
     return 0;
+}
 
+double getAverage(int *arr, int size)
+{
+    int i, sum = 0;
+    double avg;
+    for(i=0;i<size;i++)
+    {
+        sum += arr[i];
+    }
+    avg = (double)sum / size;
+    return avg;
 }
