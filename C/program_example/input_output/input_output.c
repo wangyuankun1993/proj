b7440e3281934139e963dd7e689fa9340b534340
wangyuankun <wangyuankun@aliyun.com>
2025-01-15 23:44:27 +0800

    Updated input_output logic.

--- C/program_example/input_output/input_output.c
+++ C/program_example/input_output/input_output.c
@@ -3,12 +3,26 @@
 // File Name       : input_output.c
 // Author          : Yuankun.Wang
 // Created Time    : 2025-01-13 23:44:37
-// Last Modified   : 2025-01-14 23:04:51
+// Last Modified   : 2025-01-15 23:41:55
 #include <stdio.h>
 
 int main()
 {
-    char str[] = "Hello, World!";
-    fputs(str, stdout);
+    FILE *file;
+    file = fopen("example.txt", "w");
+    if (file != NULL)
+    {
+        fprintf(file, "Hello, world!\n");
+        fclose(file);
+    }
+
+    char buffer[100];
+    file = fopen("example.txt", "r");
+    if (file != NULL)
+    {
+        fscanf(file, "%s", buffer);
+        printf("Read from file: %s\n", buffer);
+        fclose(file);
+    }
     return 0;
 }
