a17359c71364a32c8f08ce426978aac07910f67a
wangyuankun <wangyuankun@aliyun.com>
2025-01-17 23:35:16 +0800

    Updated input_output logic.

--- C/program_example/input_output/input_output.c
+++ C/program_example/input_output/input_output.c
@@ -3,14 +3,22 @@
 // File Name       : input_output.c
 // Author          : Yuankun.Wang
 // Created Time    : 2025-01-13 23:44:37
-// Last Modified   : 2025-01-16 23:02:58
+// Last Modified   : 2025-01-17 23:28:50
 #include <stdio.h>
 
 int main()
 {
     FILE *fp = NULL;
-    fp = fopen("/home/wangyuankun/proj/C/program_example/input_output/test.txt", "w+");
-    fprintf(fp, "This is testing for fprintf...\n");
-    fputs("This is testing for fputs...\n", fp);
+    char buff[255];
+
+    fp = fopen("/home/wangyuankun/proj/C/program_example/input_output/test.txt", "r");
+    fscanf(fp, "%s", buff);
+    printf("1: %s\n", buff);
+
+    fgets(buff, 255, (FILE*)fp);
+    printf("2: %s\n", buff);
+
+    fgets(buff, 255, (FILE*)fp);
+    printf("3: %s\n", buff);
     fclose(fp);
 }
