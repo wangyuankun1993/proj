b848ae97043461a5d08ee8fff11c0900660e2bfe
wangyuankun <wangyuankun@aliyun.com>
2025-01-20 23:29:29 +0800

    Added type transfer.

---
+++ C/program_example/type_trans/type.c
@@ -0,0 +1,16 @@
+// /usr/bin/gcc
+
+// File Name       : type.c
+// Author          : Yuankun.Wang
+// Created Time    : 2025-01-20 23:26:42
+// Last Modified   : 2025-01-20 23:28:08
+#include <stdio.h>
+
+int main()
+{
+    int sum = 17, count = 5;
+    double mean;
+    mean = (double) sum / count;
+    printf("Value of mean: %f\n", mean);
+    return 0;
+}
