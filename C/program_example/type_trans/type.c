e3c0b69160650d291b3a143f5bb5077a83c35454
wangyuankun <wangyuankun@aliyun.com>
2025-01-21 23:23:19 +0800

    Updated type_transfer.

--- C/program_example/type_trans/type.c
+++ C/program_example/type_trans/type.c
@@ -3,14 +3,14 @@
 // File Name       : type.c
 // Author          : Yuankun.Wang
 // Created Time    : 2025-01-20 23:26:42
-// Last Modified   : 2025-01-20 23:28:08
+// Last Modified   : 2025-01-21 23:16:04
 #include <stdio.h>
 
 int main()
 {
-    int sum = 17, count = 5;
-    double mean;
-    mean = (double) sum / count;
-    printf("Value of mean: %f\n", mean);
-    return 0;
+    int i = 17;
+    char c = 'c';
+    int sum;
+    sum = i + c;
+    printf("Value of sum: %d\n", sum);
 }
