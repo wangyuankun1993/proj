a4e13675c5e2fc4fc497d199032cff4226679756
wangyuankun <wangyuankun@aliyun.com>
2025-03-08 22:37:14 +0800

    Improved bubble sort.

--- C/program_example/sorting_algorithm/bubble_sort.c
+++ C/program_example/sorting_algorithm/bubble_sort.c
@@ -3,14 +3,14 @@
 // File Name       : bubble_sort.c
 // Author          : Yuankun.Wang
 // Created Time    : 2025-02-22 21:40:05
-// Last Modified   : 2025-02-22 22:22:30
+// Last Modified   : 2025-03-08 22:35:03
 #include <stdio.h>
 
 void bubble_sort(int arr[], int len);
 
 int main()
 {
-    int arr[] = { 22, 34, 3, 32, 82, 55, 89, 50, 37, 5, 64, 35, 9, 70 };
+    int arr[] = { 2, 1, 3, 4, 5, 6 };
     int len = sizeof(arr) / sizeof(arr[0]);
 
     bubble_sort(arr, len);
@@ -26,6 +26,7 @@ void bubble_sort(int arr[], int len)
 {
     for(int i=0; i<len-1; i++) // count
     {
+        int flag = 0;
         for(int j=0; j<len-1-i; j++) // array index
         {
             if(arr[j] > arr[j+1])
@@ -33,7 +34,13 @@ void bubble_sort(int arr[], int len)
                 int temp = arr[j+1];
                 arr[j+1] = arr[j];
                 arr[j] = temp;
+                flag = 1;
             }
+        }
+        printf("flag value is: %d!!!\n", flag);
+        if(flag == 0)
+        {
+            break;
         }
     }
 }
