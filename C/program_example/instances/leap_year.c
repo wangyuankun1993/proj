06aa992227e484a8b6bfffaf67730ecf99c6ee4e
wangyuankun <wangyuankun@aliyun.com>
2026-01-07 22:35:33 +0800

    Updated leap year.

--- C/program_example/instances/leap_year.c
+++ C/program_example/instances/leap_year.c
@@ -3,5 +3,20 @@
 // File Name       : leap_year.c
 // Author          : Yuankun.Wang
 // Created Time    : 2026-01-06 22:42:51
-// Last Modified   : 2026-01-06 22:44:16
+// Last Modified   : 2026-01-07 22:35:16
+#include <stdio.h>
 
+int isLeapYear(int year) {
+    if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) {
+        return 1;
+    }
+    return 0;
+}
+
+int main() {
+    int year, month, day;
+    int daysInMonth[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    int totalDays = 0;
+
+    printf()
+}
