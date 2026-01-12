feb0612930ad99bcdfb517ea5d04566318fd4b6d
wangyuankun <wangyuankun@aliyun.com>
2026-01-12 22:32:32 +0800

    Updated multiplication table.

--- C/program_example/instances/multiplication_table.c
+++ C/program_example/instances/multiplication_table.c
@@ -3,14 +3,14 @@
 // File Name       : multiplication_table.c
 // Author          : Yuankun.Wang
 // Created Time    : 2025-07-06 17:49:03
-// Last Modified   : 2025-07-06 17:54:17
+// Last Modified   : 2026-01-12 22:31:23
 #include <stdio.h>
 
 int main() {
     int i, j;
     for (i=1; i<=9; i++) {
         for (j=1; j<=i; j++) {
-            printf("%d x %d = %d\t", i, j, i*j);
+            printf("%d * %d = %-3d", i, j, i*j);
         }
         printf("\n");
     }
